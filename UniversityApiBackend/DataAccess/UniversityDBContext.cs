34bc19da1f70e8d524a387b11dc663364cd02115
RuiJ72 <rlagos900@gmail.com>
2022-11-08 00:15:49 -0300

    Class course created

--- UniversityApiBackend/DataAccess/UniversityDBContext.cs
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -12,5 +12,10 @@ namespace UniversityApiBackend.DataAccess
 
         //TODO: Add DbSets ( Tables of DB )
         public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Student>? Students { get; set; }
+       
     }
 }
---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+//using Microsoft.Build.Framework;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    
+    public enum Level
+    {
+        Basic,
+        Medium,
+        Advanced,
+        Hard
+    }
+
+    public class Course : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(200)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        
+        public Level Level { get; set; } = Level.Basic;
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        [Required]
+        public Chapter Chapter { get; set; } = new Chapter();
+
+        [Required]
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
